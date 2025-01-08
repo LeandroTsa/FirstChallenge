df2a12a015cc708b516a6cb0f789fd49a2c529d1
Leandro Testa <lean13590@gmail.com>
2025-01-07 22:26:12 -0300

    first upload

---
+++ Desafio1/Phone.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Phone
+    {
+        private string Model;
+        private string Brand;
+        private string phoneNumber;
+        private int operatorCode;
+
+        public Phone(string model, string brand)
+        {
+            this.Model = model;
+            this.Brand = brand;
+        }
+
+        public int opCode{ set { operatorCode = value; } }
+        public int OpCode
+        
+        {
+
+            set
+            {
+
+                if (value > 0 && value < 4)
+                {
+                    operatorCode = value;
+                }
+                else
+                {
+                    operatorCode = 0;
+                }
+
+            }
+            get { return operatorCode; }
+
+        }
+        public string phoneNum { get { return phoneNumber; }
+            set { 
+                phoneNumber = value;
+            }
+        }
+        public string brand { get {
+                return Brand;
+            } }
+        public string model
+        {
+            get
+            {
+                return Model;
+            }
+        }
+
+            public string Call()
+        {
+            return "calling to: ";
+        }
+        public string Call(string contact)
+        {
+            return Call() + contact;
+        }
+
+    }
+
+}
---
+++ Desafio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Phone number0 = new Phone("50 SE", "TCL");
+            number0.OpCode = 2;
+            Console.WriteLine("Your operator code is: " + number0.OpCode);
+            Console.WriteLine("If your operator code is showed, that's right; else: (0)");
+            Console.WriteLine("Brand of your phone is: " + number0.brand);
+            Console.WriteLine("Model of your phone is: " + number0.model);
+            number0.phoneNum = "11323345345";
+            Console.WriteLine("Your phone number is: " + number0.phoneNum);
+           Console.WriteLine(number0.Call("Martin"));
+            Console.ReadKey();
+
+
+            
+        }
+
+    }
+}
